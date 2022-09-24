a8d7c469b34aced885b5c8ded2049c86d69eb5c6
Ben Mares <services-git-throwaway1@tensorial.com>
2022-09-24 23:57:13 +0200

    Migration code

---
+++ conda_lock/vendor/scripts/poetry/vendor_poetry.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+import random
+import shlex
+import string
+import subprocess
+
+from pathlib import Path
+from typing import Callable
+
+import typer
+
+from .migration_utils import get_latest_applied_stage, migration_stage, upgrade_to, reset_to
+
+
+app = typer.Typer()
+
+
+def str_to_stage(stage: str) -> tuple[int, ...]:
+    if stage.lower == "none":
+        return ()
+    return tuple(int(x) for x in stage.split("."))
+
+
+@migration_stage(stage=1, description="Add a migration")
+def add_migration():
+    asdf = Path("asdf")
+    random_characters = "".join(random.choices(string.ascii_letters, k=10))
+    asdf.write_text(random_characters)
+
+
+@app.command()
+def current():
+    """Print the current version of the database"""
+    latest_applied_stage = get_latest_applied_stage()
+    typer.echo(f"Current stage: {latest_applied_stage}")
+
+
+@app.command()
+def upgrade(stage: str):
+    upgrade_to(str_to_stage(stage))
+
+
+@app.command()
+def reset(stage: str):
+    reset_to(str_to_stage(stage))
+
+
+if __name__ == "__main__":
+    app()
